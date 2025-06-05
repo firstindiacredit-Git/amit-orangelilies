e8ff98f6946de7ba6bba36dc151f31895ae39128
amit04-07-1999 <ameykumar76@gmail.com>
2025-06-05 15:25:46 +0530

    ggg

--- src/component/ContactPage.jsx
+++ src/component/ContactPage.jsx
@@ -23,7 +23,7 @@ const ContactPage = () => {
         setStatus('sending');
 
         try {
-            const response = await fetch('http://localhost:5000/api/contact', {
+            const response = await fetch('https://orangelilies-emailsender-backend.vercel.app/api/contact', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
